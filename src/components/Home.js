f01c9e821d2d1818ebfff6289093dbe230818adf
Fatima400 <fkhalil440@gmail.com>
2023-08-08 10:34:43 +0200

    add our mission section

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,7 +12,7 @@ function Home() {
   return (
     <div>
       <section id="home-hero" className="home-hero">
-        <Navbar/>
+        <Navbar />
         <h1 className="home-hero-text">
           <h3>Empowering Youth</h3>
           <h5>...one paddle at a time...</h5>
@@ -21,51 +21,59 @@ function Home() {
         <div className="home-hero-image"></div>
       </section>
 
+      <section >
+        <h1 className="mission-titel">OUR MISSION</h1>
+
+        <div id="mission">
+          <div className="mission-imag"></div>
+
+          <div className="mission-text">
+
+
+            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.
+              Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis
+              sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa.
+              Vestibulum lacinia arcu eget nulla.
+            </p>
+            <div> <Link to="/About" className="mission-link">READ MORE</Link></div>
+
+          </div>
+
+
+
+        </div>
+
+      </section>
       <section id="benefits">
         <h1> TRIPS BENEFITS</h1>
         <div className="benefits-cards">
           <div className="benefits-card">
-          <div className="benefits-imag"></div>
-           <h2>Friendship</h2>
-           <p>Shared adventures foster lasting bonds and create unforgettable memories</p>
+            <div className="benefits-imag"></div>
+            <h2>Friendship</h2>
+            <p>Shared adventures foster lasting bonds and create unforgettable memories</p>
           </div>
           <div className="benefits-card" >
-          <div className="benefits-imag"></div>
-          <h2>Nature</h2>
-          <p>An immersive outdoor experience that fosters a deep appreciation for the environment</p>
+            <div className="benefits-imag"></div>
+            <h2>Nature</h2>
+            <p>An immersive outdoor experience that fosters a deep appreciation for the environment</p>
 
           </div>
           <div className="benefits-card"  >
-          <div className="benefits-imag"></div>
-          <h2>Life skills</h2>
-          <p>Learn teamwork, communication, and resilience through hands-onkayaking challenges</p>
+            <div className="benefits-imag"></div>
+            <h2>Life skills</h2>
+            <p>Learn teamwork, communication, and resilience through hands-onkayaking challenges</p>
 
           </div>
-          <div className="benefits-card"> 
-          <div className="benefits-imag"></div>
-          <h2>Confidence</h2>
-          <p>Master kayaking to boost self-trust and readiness to tackle new challenges</p>
+          <div className="benefits-card">
+            <div className="benefits-imag"></div>
+            <h2>Confidence</h2>
+            <p>Master kayaking to boost self-trust and readiness to tackle new challenges</p>
 
           </div>
         </div>
       </section>
 
-      <section id="about">
-        <div className="about-imag"></div>
-        <div className="about-text"> 
-        <h1>About us</h1>
-      
-        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.
-        Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis
-        sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa.
-        Vestibulum lacinia arcu eget nulla.
-        </p>
-        <div> <Link to="/About" className="about-link">READ MORE</Link></div>
-       
 
-        </div>
-    
-      </section>
 
       <section id="tours">
         <h1>Trips and upcoming trips</h1>
@@ -83,25 +91,25 @@ function Home() {
               </div>
             );
           })}
-          
+
       </section>
 
       <section id="reviews">
-        <h1>WHAT PARENTS SAY ABOUT OUR PROJECT</h1>
-         <p>‘’As a parent, seeing my child's growth through PaddleXplorer has been an absolute joy. The unique blend of outdoor adventure and character development has boosted his confidence immensely. He made great friends and learned invaluable life skills - all while having a ton of fun on the water. I couldn't recommend PaddleXplorer more!’’</p>
-          
+        <h1 className="reviews-titel">WHAT PARENTS SAY ABOUT OUR PROJECT</h1>
+        <p className="reviews-text">‘’As a parent, seeing my child's growth through PaddleXplorer has been an absolute joy. The unique blend of outdoor adventure and character development has boosted his confidence immensely. He made great friends and learned invaluable life skills - all while having a ton of fun on the water. I couldn't recommend PaddleXplorer more!’’</p>
+
       </section>
 
 
 
       <section id="contact">
         <h1 className="contact-h">CONTACT US</h1>
-        <p className="contact-p">Ready to embark on an unforgettable adventure? Contact us today to book <br/> your child's spot on our next kayaking journey.   </p>
+        <p className="contact-p">Ready to embark on an unforgettable adventure? Contact us today to book <br /> your child's spot on our next kayaking journey.   </p>
         <ContactForm />
-     
+
       </section>
 
-  <Footer/>
+      <Footer />
     </div>
   );
 }
